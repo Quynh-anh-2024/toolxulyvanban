90417ab34c31f34738924835d452720755605d68
Quynh-anh-2024 <tranhaigcp@gmail.com>
2026-05-08 09:48:19 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Bắt buộc phải là dấu '/' đối với Vercel
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+  base: '/', // Đường dẫn chuẩn cho Vercel
 })
